3f6df4fc9b905de9f58a182c1379d0f4a1cdf668
Yogesh S <yogesh.spu@gmail.com>
2019-04-02 21:58:24 +1300

    commiting changes -01
    Fetch URL and get API
    Create JSON file and update

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author admin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String apilink = "https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false";
+		new getapiurl(apilink);
+	}
+
+}
---
+++ src/getapiurl.java
@@ -0,0 +1,64 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author admin
+ *
+ */
+public class getapiurl {
+	String urlAddress = null;
+
+	public getapiurl(String urladd) {
+		this.urlAddress = urladd;
+		geturlbackground();
+	}
+
+	private void geturlbackground() {
+		String textread = "";
+		try {
+			URL url = new URL(this.urlAddress);
+			try {
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.connect();
+				int responsecode = conn.getResponseCode();
+				if (responsecode != 200)
+					throw new RuntimeException("HttpResponseCode: " + responsecode);
+				else {
+					Scanner sc = new Scanner(url.openStream());
+					while (sc.hasNext()) {
+						textread += sc.nextLine();
+					}
+					System.out.println("\n JSON data in string format");
+					sc.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} //
+
+		try {
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter("./sampleapi.json"));
+			writer.write(textread);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
